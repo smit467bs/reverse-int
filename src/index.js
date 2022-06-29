99d5e8fb8652e409f6e81d0fa47e2ebfd3ce9bf7
Vyacheslav Byrdaev <smit467bs@gmail.com>
2022-06-29 08:50:47 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var str = String(Math.abs(n));
+    var resultStr = '';
+
+    for (var i = str.length - 1; i >= 0; --i) {
+        resultStr += `${str[i]}`;
+    }
+
+    return +resultStr;
 }
